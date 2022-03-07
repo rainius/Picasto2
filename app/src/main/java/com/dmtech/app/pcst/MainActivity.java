f3356b10e57a0e2ade008ed02439524fe66f7821
Zhang Jing <jingz.bit@gmail.com>
2022-03-07 22:11:37 +0800

    import design style

--- app/src/main/java/com/dmtech/app/pcst/MainActivity.java
+++ app/src/main/java/com/dmtech/app/pcst/MainActivity.java
@@ -1,8 +1,11 @@
 package com.dmtech.app.pcst;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //获取标题文本视图
+        TextView hw = findViewById(R.id.tv_hw);
+        //从assets创建字体对象
+        Typeface typeface = Typeface.createFromAsset(
+                getAssets(), "Lucida Calligraphy.ttf");
+        //为文本视图设置字体
+        hw.setTypeface(typeface);
+
     }
 }
