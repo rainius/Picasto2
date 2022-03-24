851dbf33a9fde26b86445621634061ba7f3fc4a7
Zhang Jing <jingz.bit@gmail.com>
2022-03-24 17:03:39 +0800

    setup nav

--- app/src/main/java/com/dmtech/app/pcst/MainActivity.java
+++ app/src/main/java/com/dmtech/app/pcst/MainActivity.java
@@ -14,13 +14,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //获取标题文本视图
-        TextView hw = findViewById(R.id.tv_hw);
-        //从assets创建字体对象
-        Typeface typeface = Typeface.createFromAsset(
-                getAssets(), "Lucida Calligraphy.ttf");
-        //为文本视图设置字体
-        hw.setTypeface(typeface);
-
     }
 }
