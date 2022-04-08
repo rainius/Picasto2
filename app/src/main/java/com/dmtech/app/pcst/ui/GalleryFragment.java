dacdd7754f31101d1aa3aab6e6f9be1a8cbf29a1
Zhang Jing <jingz.bit@gmail.com>
2022-04-08 12:56:19 +0800

    navigation

---
+++ app/src/main/java/com/dmtech/app/pcst/ui/GalleryFragment.java
@@ -0,0 +1,38 @@
+package com.dmtech.app.pcst.ui;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dmtech.app.pcst.R;
+
+public class GalleryFragment extends Fragment {
+
+    private GalleryViewModel mViewModel;
+
+    public static GalleryFragment newInstance() {
+        return new GalleryFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.gallery_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(GalleryViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/dmtech/app/pcst/ui/GalleryViewModel.java
@@ -0,0 +1,7 @@
+package com.dmtech.app.pcst.ui;
+
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
