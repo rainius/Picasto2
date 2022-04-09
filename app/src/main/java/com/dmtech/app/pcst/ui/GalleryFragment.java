f876771c9d91b9ffecfd3b4a9837acca55d02de0
Zhang Jing <jingz.bit@gmail.com>
2022-04-09 22:50:19 +0800

    Gallery Fragment

--- app/src/main/java/com/dmtech/app/pcst/ui/GalleryFragment.java
+++ app/src/main/java/com/dmtech/app/pcst/ui/GalleryFragment.java
@@ -1,5 +1,6 @@
 package com.dmtech.app.pcst.ui;
 
+import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -11,8 +12,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.dmtech.app.pcst.R;
+import com.dmtech.app.pcst.util.TypefaceUtil;
 
 public class GalleryFragment extends Fragment {
 
@@ -25,7 +28,19 @@ public class GalleryFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.gallery_fragment, container, false);
+        View view = inflater.inflate(R.layout.gallery_fragment, container, false);
+
+        Toolbar toolbar = view.findViewById(R.id.toolbar_gallery);
+        initToolbar(toolbar);
+        return view;
+    }
+
+    //为主标题设定字体
+    private void initToolbar(Toolbar toolbar) {
+        TextView titleView = (TextView) toolbar.getChildAt(0);
+        if (titleView != null) {
+            TypefaceUtil.setTypeface(titleView, "Lucida Calligraphy.ttf");
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/dmtech/app/pcst/ui/PostsViewModel.java
@@ -0,0 +1,7 @@
+package com.dmtech.app.pcst.ui;
+
+import androidx.lifecycle.ViewModel;
+
+public class PostsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
