187d618af95d6a6efd0e7ba4a41f4f048bd3a3a0
Zhang Jing <jingz.bit@gmail.com>
2022-05-10 08:23:17 +0800

    temp

--- app/src/main/java/com/dmtech/app/pcst/MainActivity.java
+++ app/src/main/java/com/dmtech/app/pcst/MainActivity.java
@@ -9,17 +9,41 @@ import androidx.navigation.ui.NavigationUI;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.dmtech.app.pcst.databinding.ActivityMainBinding;
+import com.dmtech.app.pcst.picsel.GlideEngine;
 import com.dmtech.app.pcst.util.SessionUtil;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.luck.picture.lib.basic.PictureSelector;
+import com.luck.picture.lib.config.SelectMimeType;
+import com.luck.picture.lib.entity.LocalMedia;
+import com.luck.picture.lib.interfaces.OnResultCallbackListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    //视图绑定对象
+    ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //使用视图绑定以操作布局中各视图元素
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+//        setContentView(R.layout.activity_main);
+        setContentView(mBinding.getRoot());
+        // 点击发帖按钮
+        mBinding.ivAddPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPictureSelector();
+            }
+        });
+
         //获取导航宿主Fragment
         NavHostFragment navHostFragment =
                 (NavHostFragment) getSupportFragmentManager()
@@ -31,5 +55,23 @@ public class MainActivity extends AppCompatActivity {
         //实现导航
         NavigationUI.setupWithNavController(navView, navController);
         Log.d("Picasto", "user: " + SessionUtil.getUsername(this));
+
+    }
+
+    private void showPictureSelector() {
+        PictureSelector.create(this)
+                .openGallery(SelectMimeType.ofImage())
+                .setImageEngine(GlideEngine.createGlideEngine())
+                .forResult(new OnResultCallbackListener<LocalMedia>() {
+                    @Override
+                    public void onResult(ArrayList<LocalMedia> result) {
+
+                    }
+
+                    @Override
+                    public void onCancel() {
+
+                    }
+                });
     }
 }
