26079f653cea1fd3fd740cadb29cd0eb676d4381
Zhang Jing <jingz.bit@gmail.com>
2023-04-20 09:42:55 +0800

    post list complet

---
+++ app/src/main/java/com/dmtech/app/pcst/adapter/AlbumAdapter.java
@@ -0,0 +1,61 @@
+package com.dmtech.app.pcst.adapter;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.dmtech.app.pcst.R;
+import com.dmtech.app.pcst.data.Post;
+import com.dmtech.app.pcst.databinding.PostAlbumLayoutBinding;
+import com.dmtech.app.pcst.util.HttpHelper;
+
+public class AlbumAdapter extends RecyclerView.Adapter<AlbumAdapter.AlbumHolder> {
+
+    //样例图片数组
+    private static final int[] SAMPLE_PHOTOS = {
+            R.drawable.sample0,
+            R.drawable.sample1,
+            R.drawable.sample2
+    };
+
+    private Post post;
+
+    public AlbumAdapter(Post post) {
+        this.post = post;
+    }
+
+    @NonNull
+    @Override
+    public AlbumHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(
+                parent.getContext()).inflate(R.layout.post_album_layout,
+                parent,
+                false);
+        return new AlbumHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AlbumHolder holder, int position) {
+        Glide.with(holder.binding.ivAlbumPhoto)
+                .load(SAMPLE_PHOTOS[position]) //从URL加载图片
+                .into(holder.binding.ivAlbumPhoto);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+
+    public static class AlbumHolder extends RecyclerView.ViewHolder {
+        PostAlbumLayoutBinding binding;
+        public AlbumHolder(@NonNull View itemView) {
+            super(itemView);
+            binding = PostAlbumLayoutBinding.bind(itemView);
+        }
+    }
+}
--- app/src/main/java/com/dmtech/app/pcst/adapter/PostAdapter.java
+++ app/src/main/java/com/dmtech/app/pcst/adapter/PostAdapter.java
@@ -1,5 +1,6 @@
 package com.dmtech.app.pcst.adapter;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,9 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.dmtech.app.pcst.R;
 import com.dmtech.app.pcst.data.Post;
 import com.dmtech.app.pcst.databinding.PostItemLayoutBinding;
+import com.dmtech.app.pcst.util.HttpHelper;
 
 import java.util.List;
 //相册页帖子列表适配器
@@ -41,6 +44,28 @@ public class PostAdapter
         Post post = posts.getValue().get(position);
         //设定作者昵称
         holder.binding.tvPostAuthor.setText(post.getAuthor());
+        //绑定用户头像
+        Glide.with(holder.binding.ivAuthorHead)
+                .load(R.drawable.logo_placeholder)//获取头像URL后加载
+                .into(holder.binding.ivAuthorHead);
+        //为VP2关联图集
+        holder.binding.vp2Album.setAdapter(new AlbumAdapter(post));
+        //处理点赞相关操作
+        holder.binding.ivLike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Posts", "点赞");
+                post.setLike(!post.isLike());
+                notifyDataSetChanged();
+                //联网同步状态
+            }
+        });
+        //根据帖子点赞状态设定图标外观
+        if (post.isLike()) {
+            holder.binding.ivLike.setImageResource(R.drawable.ic_like_fill);
+        } else {
+            holder.binding.ivLike.setImageResource(R.drawable.ic_like_line);
+        }
     }
 
     @Override
